8e4aa9d9bccc690f5ae0183ebe4b2c7b6a1f0dd4
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-07-12 00:01:16 -0400

    started book work

---
+++ src/main/java/ic2c_doc_book/IC2CDocBook.java
@@ -0,0 +1,12 @@
+package ic2c_doc_book;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = IC2CDocBook.MODID, name = IC2CDocBook.MOD_NAME, version = IC2CDocBook.VERSION, dependencies = IC2CDocBook.DEPENDENCIES)
+public class IC2CDocBook{
+    public static final String MODID = "ic2c_doc_book";
+    public static final String MOD_NAME = "Ic2c Documentation Book";
+    public static final String VERSION = "@VERSION@";
+    public static final String DEPENDENCIES = "required-after:ic2-classic-spmod;required-after:patchouli";
+
+}
---
+++ src/main/resources/assets/ic2c_doc_book/readme.txt
@@ -0,0 +1,3 @@
+To use the custom image, use the RESOURCE LOADER at https://www.curseforge.com/minecraft/mc-mods/resource-loader
+
+The quest page uses an advancement from Botania, so it will have to be loaded.
